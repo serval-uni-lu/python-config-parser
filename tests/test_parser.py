bdd85bb6cc0286bf169d51e2667e90e3e73a5e2c
Thibault Simonetto <thibault.simonetto.001@student.uni.lu>
2022-01-18 14:14:18 +0100

    Update tests

---
+++ tests/test_parser.py
@@ -0,0 +1,44 @@
+import pytest
+
+from config_parser.parser import InlineJsonParser, StrParser, ConfigFileParser
+
+
+@pytest.mark.parametrize(
+    "str_input,dic_out",
+    [
+        ("key1=abc", {"key1": "abc"}),
+        ("key1.key2=3", {"key1": {"key2": 3}}),
+        ("key1=", None),
+    ],
+)
+def test_str_parser(str_input, dic_out):
+    str_parser = StrParser()
+    res = str_parser.do(f"{str_input}")
+    assert res == dic_out
+
+
+@pytest.mark.parametrize(
+    "inline_input, dic_out",
+    [
+        ("{" + f'"key1":3,"key2":3' + "}", {"key1": 3, "key2": 3}),
+        ("{" + f'"key1":,"key2":3' + "}", None),
+    ],
+)
+def test_inline_parser(inline_input, dic_out):
+    inline_parser = InlineJsonParser()
+    res = inline_parser.do(inline_input)
+    assert res == dic_out
+
+
+@pytest.mark.parametrize(
+    "config_path,dic_out",
+    [
+        ("./tests/test_config.yaml", {"a": {"b": "c"}}),
+        ("./tests/test_config.yml", {"a": {"b": "c"}}),
+        ("./tests/test_config.json", {"a": {"b": "c"}}),
+    ],
+)
+def test_yaml_file(config_path, dic_out):
+    file_parser = ConfigFileParser()
+    res = file_parser.do(config_path)
+    assert res == dic_out
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,8 +1,10 @@
-from config_parser.utils import *
+from config_parser.utils import get_config, get_dict_hash
 
 
 def test_get_dict_hash_sample():
-    assert get_dict_hash({"a": 1, "b": 2}) == "8aacdb17187e6acf2b175d4aa08d7213"
+    assert (
+        get_dict_hash({"a": 1, "b": 2}) == "8aacdb17187e6acf2b175d4aa08d7213"
+    )
 
 
 def test_get_dict_hash_order():
@@ -10,5 +12,5 @@ def test_get_dict_hash_order():
 
 
 def test_getConfig():
-    config = get_config()
+    _ = get_config()
     assert True
